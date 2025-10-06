3f6c9b0ab4bf25caacaa9c7049073992b3ce7924
ibattistax <isabel.battista@gmail.com>
2025-10-06 11:04:15 -0600

    Minor edits to content

--- src/app.js
+++ src/app.js
@@ -25,32 +25,3 @@ document.addEventListener("scroll", () => {
     }
   });
 });
-
-let btn = document.querySelector("#btn");
-btn.addEventListener("click", function (e) {
-  e.preventDefault();
-  let name = document.querySelector("#name").value;
-  let email = document.querySelector("#email").value;
-  let subject = document.querySelector("#subject").value;
-  let message = document.querySelector("#message").value;
-  let body =
-    "name: " +
-    name +
-    "<br /> email: " +
-    email +
-    "<br /> subject: " +
-    subject +
-    "<br /> message: " +
-    message;
-
-  Email.send({
-    // SecureToken: "7abfcf08-d7ce-4dd3-9f42-2a276c4cfcc5",
-    Host: "smtp.elasticemail.com",
-    Username: "lilly.golden@gmail.com",
-    Password: "7EDBA97E71EF710AC86B83DFCEAFC11365FA",
-    To: "lilly.golden@gmail.com",
-    From: "lilly.golden@gmail.com",
-    Subject: subject,
-    Body: body,
-  }).then((message) => alert(message));
-});
